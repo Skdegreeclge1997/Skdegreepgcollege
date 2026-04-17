eb6afaf9e1f46be82cc49fd0f7c984fbb1012c78
Vasetti Janakirao <janakirao4701@gmail.com>
2026-04-17 11:18:58 +0530

    fix: import Image in Footer to resolve build error

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { Mail, MapPin, Phone, MessageSquare, Globe, User } from 'lucide-react';
 
 export default function Footer() {
